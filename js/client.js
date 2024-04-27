50f02679c01a0c5d3536f7d49c6ed6fb438442a8
manikandan2k29 <manikandanthangapandiyan29@gmail.com>
2024-04-27 09:18:41 +0530

    feature(no story): added remote video

--- js/client.js
+++ js/client.js
@@ -1,61 +1,103 @@
-
 let localPeerConnection;
 let remotePeerConnection;
+let localVideo = document.getElementById("localVideo");
+let remoteVideo = document.getElementById("remoteVideo");
+let localStream;
+let remoteStream;
+
+
+navigator.mediaDevices
+  .getUserMedia({
+    audio: false,
+    video: true,
+  })
+  .then((stream) => {
+    localVideo.srcObject = stream;
+    localStream = stream;
+  });
 
 
 function generateSession() {
   localPeerConnection = new RTCPeerConnection();
   localPeerConnection.onicecandidate = (e) => {
     console.log("New Candidate");
+    document.getElementById("offer").innerText = JSON.stringify(localPeerConnection.localDescription);
     console.log(JSON.stringify(localPeerConnection.localDescription));
   };
+ 
+  localStream.getTracks().forEach(track => {
+    localPeerConnection.addTrack(track, localStream);
+});
 
+  
   const sendChannel = localPeerConnection.createDataChannel("sendChannel");
 
   sendChannel.onmessage = (e) => console.log("messsage :  " + e.data);
   sendChannel.onopen = (e) => console.log("open");
   sendChannel.onclose = (e) => console.log("close");
-  localPeerConnection.channel = sendChannel
-  localPeerConnection.createOffer().then(o => localPeerConnection.setLocalDescription(o))
-
+  localPeerConnection.channel = sendChannel;
+  // localPeerConnection.addStream(localStream);
+  localPeerConnection
+    .createOffer()
+    .then((o) => localPeerConnection.setLocalDescription(o));
 }
 
-function setRemote(){
-  const answer = document.getElementById("connectionStringLocal").value
-  localPeerConnection.setRemoteDescription(JSON.parse(answer)).then(a=>console.log("set remote desc"))
+function setRemote() {
+  const answer = document.getElementById("connectionStringLocal").value;
+  localPeerConnection
+    .setRemoteDescription(JSON.parse(answer))
+    .then((a) => console.log("set remote desc"));
 }
 
 function answer() {
-  const offer = document.getElementById("connectionString").value
+  const offer = document.getElementById("connectionString").value;
   console.log(offer);
   remotePeerConnection = new RTCPeerConnection();
   remotePeerConnection.onicecandidate = (e) => {
     console.log("New Candidate");
-    console.log(remotePeerConnection.localDescription);
+    document.getElementById("answer").innerText = JSON.stringify(remotePeerConnection.localDescription);
+    console.log(JSON.stringify(remotePeerConnection.localDescription));
   };
 
-  remotePeerConnection.ondatachannel= e =>{
+  remotePeerConnection.ondatachannel = (e) => {
     const receiveChannel = e.channel;
     receiveChannel.onmessage = (e) => console.log("client :  " + e.data);
     receiveChannel.onopen = (e) => console.log("open");
     receiveChannel.onclose = (e) => console.log("close");
-    remotePeerConnection.channel = receiveChannel
-  }
+    remotePeerConnection.channel = receiveChannel;
+  };
+  // remotePeerConnection.track = (e)=>{
+  //   console.log(e);
+  //   remoteStream = e;
+  //   remoteVideo.srcObject = e;
+  //   console.log("added remote video");
+  // }
+  remotePeerConnection.addEventListener('track', async (event) => {
+    const [remoteStream] = event.streams;
+    remoteVideo.srcObject = remoteStream;
+    console.log("added remote video");
 
- 
-  remotePeerConnection.setRemoteDescription(JSON.parse(offer)).then(a=>console.log("set remote desc"))
+});
 
+  remotePeerConnection
+    .setRemoteDescription(JSON.parse(offer))
+    .then((a) => console.log("set remote desc"));
 
-  remotePeerConnection.createAnswer().then(o => remotePeerConnection.setLocalDescription(o)).then(o => console.log(JSON.stringify(remotePeerConnection.localDescription)))
+  remotePeerConnection
+    .createAnswer()
+    .then((o) => remotePeerConnection.setLocalDescription(o))
+    .then((o) =>
+      console.log(JSON.stringify(remotePeerConnection.localDescription))
+    );
   console.log(remotePeerConnection);
 }
 
-function send(){
+function send() {
   const messageToSend = document.getElementById("message").value;
-  if(localPeerConnection == undefined){
-    remotePeerConnection.channel.send(messageToSend)
-  }else{
-    localPeerConnection.channel.send(messageToSend)
+  if (localPeerConnection == undefined) {
+    remotePeerConnection.channel.send(messageToSend);
+  } else {
+    localPeerConnection.channel.send(messageToSend);
   }
- console.log("you :  "+ messageToSend);
+  console.log("you :  " + messageToSend);
 }
